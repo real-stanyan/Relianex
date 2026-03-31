de94b274a0eb22cc65bc4042933aed6676f62832
stan yan <stanhavenoidea@gmail.com>
2026-03-31 22:12:29 +1100

    vercel fix

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+  // @ts-ignore
   eslint: {
-    // 警告：这会让你的项目即使有 ESLint 错误也能打包成功
     ignoreDuringBuilds: true,
   },
 };
